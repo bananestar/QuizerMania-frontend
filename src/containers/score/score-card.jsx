1f3f16355791ef47aee3a18bf176bdfacd2f63e9
bananestar <s.vanderlinden13@gmail.com>
2022-09-23 11:22:52 +0200

    score card

---
+++ src/containers/score/score-card.jsx
@@ -0,0 +1,24 @@
+import { Card, CardActionArea, CardContent, CardMedia, Typography } from '@mui/material';
+import { Link } from 'react-router-dom';
+
+const ScoreCard = ({ quiz }) => {
+	console.log(quiz);
+	return (
+		<Card sx={{ maxWidth: 345 }}>
+			<CardActionArea component={Link} to={'/score/'+ quiz.quizID}>
+				<CardMedia
+                sx={{padding: 1}}
+					component="img"
+					image="https://firebasestorage.googleapis.com/v0/b/quizermania-f7be1.appspot.com/o/score.png?alt=media&token=0d8bf460-925b-4f0c-9e0d-c5880b4578a5"
+					alt="green iguana"
+				/>
+				<CardContent>
+					<Typography gutterBottom variant="h5" component="div">
+						{quiz.name}
+					</Typography>
+				</CardContent>
+			</CardActionArea>
+		</Card>
+	);
+};
+export default ScoreCard;
--- src/pages/score/score.jsx
+++ src/pages/score/score.jsx
@@ -1,5 +1,54 @@
-const ScorePage = () =>{
-    return <>score</>
-}
+import { Alert, CircularProgress, Grid } from '@mui/material';
+import { Box } from '@mui/system';
+import ScoreCard from '../../containers/score/score-card';
+import { useQuery } from '../../hooks/useRequest';
 
-export default ScorePage
+const ScorePage = () => {
+	const URL = import.meta.env.VITE_API_SCORE;
+	const { data, isLoading, errors } = useQuery(URL);
+	if (isLoading) {
+		return <CircularProgress />;
+	}
+	if (errors) {
+		console.error('-------------ERROR------------');
+		console.error(errors);
+		console.error('-------------ERROR------------');
+		return (
+			<Alert margin="dense" severity="error">
+				{errors}
+			</Alert>
+		);
+	}
+	if (data) {
+		const quiz = data.results.map((e) => {
+			return e;
+		});
+		return (
+			<Box sx={{ marginLeft: 30, flexGrow: 1 }}>
+				<Grid container >
+					{quiz.map((e) => {
+						return (
+							<>
+								&nbsp; &nbsp; &nbsp;
+								<Grid
+									item
+									sx={{
+										width: 275,
+										boxShadow: 6,
+										':hover': {
+											boxShadow: 20,
+										},
+									}}
+								>
+                                    <ScoreCard quiz={e} />
+                                </Grid>
+							</>
+						);
+					})}
+				</Grid>
+			</Box>
+		);
+	}
+};
+
+export default ScorePage;
