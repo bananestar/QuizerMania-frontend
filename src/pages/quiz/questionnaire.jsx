0e7021dd625bbb574f41c52c5df5ecb9adf9bac0
bananestar <s.vanderlinden13@gmail.com>
2022-09-23 11:30:58 +0200

    secu questionnaire

--- src/pages/quiz/questionnaire.jsx
+++ src/pages/quiz/questionnaire.jsx
@@ -1,13 +1,16 @@
-import { Alert, CircularProgress, Typography } from '@mui/material';
+import { Alert, Button, CircularProgress, Typography } from '@mui/material';
 import { Box } from '@mui/system';
 import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
+import { useRecoilState } from 'recoil';
+import { jwtAtom } from '../../atoms/jwtAtom';
 import QuizCardQuestion from '../../containers/quiz/quiz-card-question';
 import { useQuery } from '../../hooks/useRequest';
 
 const QuestionnairePage = () => {
 	const { quizID } = useParams();
 	const URL = import.meta.env.VITE_API_QUIZ + 'allQuestionQuiz/';
+	const [token, setToken] = useRecoilState(jwtAtom);
 
 	const { data, isLoading, errors } = useQuery(URL, quizID);
 
@@ -25,12 +28,29 @@ const QuestionnairePage = () => {
 		);
 	}
 
+	if (!token) {
+		return (
+			<Box align={'center'}>
+				<h1>Vous devez être connecter</h1>
+				<Button component={Link} to='/registration' variant="contained">
+					Sign Up
+				</Button>
+				&nbsp; &nbsp; &nbsp;
+				<Button component={Link} to='/login' variant="contained">
+					Login
+				</Button>
+			</Box>
+		);
+	}
+
 	if (data) {
-        const quiz = data.result[0]
-        const questions = quiz.questions
-		return <Box align={'center'} >
-            <QuizCardQuestion questions={questions} quiz={quiz} />
-        </Box>;
+		const quiz = data.result[0];
+		const questions = quiz.questions;
+		return (
+			<Box align={'center'}>
+				<QuizCardQuestion questions={questions} quiz={quiz} />
+			</Box>
+		);
 	}
 };
 export default QuestionnairePage;
